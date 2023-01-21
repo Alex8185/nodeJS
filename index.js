2c5ed5eeaadbc57af7ffe077b3d2ffce01732de3
Suboch_Alex <eclipse85@tut.by>
2023-01-21 21:19:02 +0300

    day_2

--- index.js
+++
@@ -1,7 +0,0 @@
-const CC = require('currency-converter-lt')
-let currencyConverter = new CC({from:"USD", to:"EUR", amount:100})
-
-currencyConverter.convert().then((response) => {
-    console.log('Result: ${response}') //or do something else
-})
-
