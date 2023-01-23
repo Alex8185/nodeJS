1ef9c0040b80146c1e242ac06cefca394cd01b9f
Suboch_Alex <eclipse85@tut.by>
2023-01-23 23:27:42 +0300

    4.1

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const mySity = {
-    city: 'New York',
-    cityGreeting () {
-        console.log('Greetings!!')
-    }
+const person = {
+    name: 'Alex',
+    age: 40
 }
 
-mySity.cityGreeting()
+const person2 = Object.assign({},person)
+
+person2.age = 42
+
+console.log(person.age)
+console.log(person2.age)
