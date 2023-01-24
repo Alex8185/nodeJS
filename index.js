a2a93c0ab49eca92d8628a587f71b50aa5446c40
Suboch_Alex <eclipse85@tut.by>
2023-01-24 16:13:03 +0300

    5.0

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-const personOne = {
-    name: 'Alex',
-    age: 20
+const a = 5
+function myFn() {
+    function innerFn () {
+        console.log(a)
+    }
+    innerFn()
 }
-
-function increasePersonAge (person) {
-    const updatePerson = Object.assign({}, person)
-    updatePerson.age += 1
-    return updatePerson
-}
-
-const updatePersonOne = increasePersonAge(personOne)
-console.log(personOne.age)
-console.log(updatePersonOne.age)
+myFn()
