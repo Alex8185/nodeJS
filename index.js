38188fdcbb3b9f528fc90afe344c5af1e04c4035
Suboch_Alex <eclipse85@tut.by>
2023-01-28 02:39:58 +0300

    6.53

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const age = 16
+const age = 25
 
 if (age > 18) {
     console.log('Is adult')
-}  else if (age >= 12) {
+}  
+
+if (age >= 12) {
     console.log('Is teenager')
-}  else {
+}  
+
+if (age <12) {
     console.log('Is child')
 }
