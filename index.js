6cda02c2457cbdafb80a04b62152fb82c00ce600
Suboch_Alex <eclipse85@tut.by>
2023-01-27 23:37:35 +0300

    6.33

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const Array = ['apple', 'banana']
-const [frutOne, frutTwo] = Array
-console.log(frutOne)
-console.log(frutTwo)
+const userProfile = {
+    name: 'Alex',
+    surname: 20,
+    age: 34
+}
+
+const newUser = ({name, surname}) => {
+    if(!surname) {
+        return `User ${name} has no comment`
+    }
+    return `User ${name} has ${surname} comments`
+}
+console.log(newUser(userProfile))
