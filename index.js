3b6d07ac265d9d9de74ca0003b3fdbdc5ac658fd
Suboch_Alex <eclipse85@tut.by>
2023-01-28 03:33:19 +0300

    7.13

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const month = 2
+const value = 11
 
-switch (month) {
-    case 12:
-        console.log('декабрь')
-        break
-    case 1:
-        console.log('январь')
-        break
-    case 2:
-        console.log('февраль')
-    break
-    default:
-        console.log('Это не зимний месяц')
-}
+value 
+    ? console.log('Условие истинно')
+    : console.log('Условие ложно')
