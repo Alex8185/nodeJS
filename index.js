b78b6b6ee48b827a7433f2ffd0fd9380075c1fae
Suboch_Alex <eclipse85@tut.by>
2023-01-28 03:40:14 +0300

    7.16

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const value = 11
-
-value 
-    ? console.log('Условие истинно')
-    : console.log('Условие ложно')
+const value1 = 11
+const value2 = 25
+    value1 && value2
+    ? myFn1(value1, value2)
+    : myFn2()
+    
