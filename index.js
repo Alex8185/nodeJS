051d848bc79cd130459230e3d3efdd3dd31bfcaa
Suboch_Alex <eclipse85@tut.by>
2023-01-28 03:49:33 +0300

    7.21

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const value1 = 11
-const value2 = 25
-    value1 && value2
-    ? myFn1(value1, value2)
-    : myFn2()
-    
+let value = 11
+console.log(value >= 0 ? value : -value)
+
+value = -5
+const res = value >= 0  ? value : -value
+console.log(res)
